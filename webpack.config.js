e086784d89e9d7859e0e5cb198d8cbe09892a7e4
Hermit Baba <suprakashb2003@gmail.com>
2026-07-24 13:30:19 +0600

    Change build output to public to match Vercel defaults

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = async (env, options) => {
       commands: "./src/commands/commands.ts",
     },
     output: {
-      path: path.resolve(__dirname, "dist"),
+      path: path.resolve(__dirname, "public"),
       filename: "[name].[contenthash].js",
       clean: true,
     },
@@ -92,7 +92,7 @@ module.exports = async (env, options) => {
       port: 3000,
       hot: true,
       static: {
-        directory: path.join(__dirname, "dist"),
+        directory: path.join(__dirname, "public"),
       },
       allowedHosts: "all",
     },
